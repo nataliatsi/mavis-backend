0ca1e6603e48b53c5ec4c00beb20b155a364ce8c
Natália S. Gomes <nataliandsg32@gmail.com>
2025-01-24 17:44:42 -0300

    Initial commit

---
+++ src/test/java/com/nataliatsi/mavis/MavisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataliatsi.mavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
