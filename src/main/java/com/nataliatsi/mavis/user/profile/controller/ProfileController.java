3a6927dd7c08bd561c61ccb4120ea9d7aa7bbf8a
Natália S. Gomes <nataliandsg32@gmail.com>
2025-02-18 17:45:36 -0300

    feat: create Profile controller and implement service for profile management.

---
+++ src/main/java/com/nataliatsi/mavis/user/profile/controller/ProfileController.java
@@ -0,0 +1,34 @@
+package com.nataliatsi.mavis.user.profile.controller;
+
+import com.nataliatsi.mavis.user.profile.dto.CreateProfileDto;
+import com.nataliatsi.mavis.user.profile.entities.Profile;
+import com.nataliatsi.mavis.user.profile.service.ProfileService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profiles")
+public class ProfileController {
+
+    private final ProfileService userProfileService;
+
+    public ProfileController(ProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Profile> registerUser(
+            @Valid @RequestBody CreateProfileDto dataUserRegister,
+            Authentication authentication) {
+
+        Profile createdUser = userProfileService.createUserProfile(dataUserRegister, authentication);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+}
