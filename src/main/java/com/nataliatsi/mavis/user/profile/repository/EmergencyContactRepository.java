834fcf961af61a5af2a4cdc5e19ea3bb75aaa180
Natália S. Gomes <nataliandsg32@gmail.com>
2025-02-19 12:40:15 -0300

    refactor: import repository annotation

--- src/main/java/com/nataliatsi/mavis/user/profile/repository/EmergencyContactRepository.java
+++ src/main/java/com/nataliatsi/mavis/user/profile/repository/EmergencyContactRepository.java
@@ -2,9 +2,11 @@ package com.nataliatsi.mavis.user.profile.repository;
 
 import com.nataliatsi.mavis.user.profile.entities.EmergencyContact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmergencyContactRepository extends JpaRepository<EmergencyContact, Long> {
     Optional<EmergencyContact> findByEmail(String email);
 }
--- src/main/java/com/nataliatsi/mavis/user/profile/repository/ProfileRepository.java
+++ src/main/java/com/nataliatsi/mavis/user/profile/repository/ProfileRepository.java
@@ -1,7 +1,14 @@
 package com.nataliatsi.mavis.user.profile.repository;
 
+import com.nataliatsi.mavis.entities.User;
 import com.nataliatsi.mavis.user.profile.entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    Optional<Profile> findByUser(User user);
 }
