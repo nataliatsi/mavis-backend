06ae7a9e1d39ad151e6bef80ecb718b13a225e96
Natália S. Gomes <nataliandsg32@gmail.com>
2025-02-28 20:42:14 -0300

    refactor: atualiza url

--- src/main/java/com/nataliatsi/mavis/controller/NotificationController.java
+++ src/main/java/com/nataliatsi/mavis/controller/NotificationController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/api/notifications")
 public class NotificationController {
 
     private final MessageSenderStrategy messageSenderStrategy;
