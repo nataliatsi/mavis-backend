628948ed40e02b2ec9d0f95e337250852b030453
Natália S. Gomes <nataliandsg32@gmail.com>
2025-06-18 17:24:30 -0300

    feat: adicionar e configurar perfil de teste

--- src/test/java/com/nataliatsi/mavis/MavisApplicationTests.java
+++ src/test/java/com/nataliatsi/mavis/MavisApplicationTests.java
@@ -2,8 +2,10 @@ package com.nataliatsi.mavis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MavisApplicationTests {
 
 	@Test
