b25d747d69997abe9a2695a1cb25581fe04c6b5e
Natália S. Gomes <nataliandsg32@gmail.com>
2025-10-01 14:49:59 -0300

    refactor: rename method, update documentation

--- src/main/java/com/nataliatsi/mavis/controller/UserController.java
+++ src/main/java/com/nataliatsi/mavis/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.nataliatsi.mavis.controller;
 
-import com.nataliatsi.mavis.dto.UserRequestDTO;
+import com.nataliatsi.mavis.dto.UserCreateDTO;
 import com.nataliatsi.mavis.entities.User;
 import com.nataliatsi.mavis.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v2/users")
 @Tag(name = "User", description = "Endpoints for user management")
 public class UserController {
     private final UserService userService;
@@ -29,7 +27,7 @@ public class UserController {
     }
 
     @Operation(
-            summary = "Register a new user",
+            summary = "Create a new user",
             description = "Creates a new user in the system based on the provided data.",
             responses = {
                     @ApiResponse(
@@ -39,20 +37,14 @@ public class UserController {
                     ),
                     @ApiResponse(
                             responseCode = "400",
-                            description = "Invalid request - invalid data in payload",
+                            description = "Bad Request - invalid data in payload or user already exists",
                             content = @Content
-                    ),
-                    @ApiResponse(
-                            responseCode = "422",
-                            description = "Business rule violation - email, phone number or username already in use",
-                            content = @Content
-                    )
-            }
+                    )}
     )
     @PostMapping
-    public ResponseEntity<?> register(@RequestBody @Valid UserRequestDTO dto) {
+    public ResponseEntity<?> createUser(@RequestBody @Valid UserCreateDTO dto) {
         User user = userService.create(dto);
-        URI location = URI.create("/api/users/" + user.getUserId());
+        URI location = URI.create("/api/v2/users/" + user.getUserId());
 
         return ResponseEntity.created(location).build();
     }
