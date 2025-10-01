8e773455c339c4fca60d58f20454c1bf25dfc679
Natália S. Gomes <nataliandsg32@gmail.com>
2025-10-01 14:49:31 -0300

    refactor: update mapper to handle new UserCreateDTO

--- src/main/java/com/nataliatsi/mavis/mapper/UserMapper.java
+++ src/main/java/com/nataliatsi/mavis/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.nataliatsi.mavis.mapper;
 
 import com.nataliatsi.mavis.dto.AddressRequestDTO;
+import com.nataliatsi.mavis.dto.UserCreateDTO;
 import com.nataliatsi.mavis.dto.UserRequestDTO;
 import com.nataliatsi.mavis.entities.Address;
 import com.nataliatsi.mavis.entities.User;
@@ -12,7 +13,7 @@ public interface UserMapper {
     @Mapping(target = "userId", ignore = true)
     @Mapping(target = "roles", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
-    User toEntity(UserRequestDTO dto);
+    User toEntity(UserCreateDTO dto);
 
     @Mapping(target = "id", ignore = true)
     Address toAddress(AddressRequestDTO dto);
