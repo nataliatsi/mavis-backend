0f94225a58e5ad14a83c3f89723b8deb987dfc7c
Natália S. Gomes <nataliandsg32@gmail.com>
2025-10-01 14:47:27 -0300

    feat: add new UserCreateDTO for user creation

---
+++ src/main/java/com/nataliatsi/mavis/dto/UserCreateDTO.java
@@ -0,0 +1,32 @@
+package com.nataliatsi.mavis.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserCreateDTO(
+        @NotBlank(message = "Username cannot be empty or null")
+        @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+        String username,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Phone number is required")
+        @Pattern(
+                regexp = "^\\+?[0-9]{10,15}$",
+                message = "Phone number must contain between 10 and 15 digits and may start with +"
+        )
+        String phoneNumber,
+
+        @NotBlank(message = "Password cannot be empty or null")
+        @Size(min = 8, message = "Password must have at least 8 characters")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+                message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        )
+        String password
+) {
+}
